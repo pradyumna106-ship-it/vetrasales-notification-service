b63d0a97964978974f90d5ad80172a1e95ebd718
pr4dyumn4 <pradyumnajekumar@gmail.com>
2025-12-15 12:19:17 +0530

    token updated

--- src/main/java/com/notification/email/TwilioConfig.java
+++ src/main/java/com/notification/email/TwilioConfig.java
@@ -7,6 +7,6 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties(prefix = "twilio")
 public class TwilioConfig {
 	public static final String ACCOUNT_SID = "AC1b686c659a729ac3a1dae9333f2425f4";
-	  public static final String AUTH_TOKEN = "54c51a6ed01e041e04d50a4b91b2ce47";
+	  public static final String AUTH_TOKEN = "f9cd2ec633b86d5e038645bdceaaf242";
 	 public static final String messagingServiceSid = "MG6696d46b7e6f3c2d4de226c24da0b398"; 
 }
